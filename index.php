cae4906715cc0c6ef60ac3a2ebae5dcb135b8ed7
ezraimo <59790107+ezraimo@users.noreply.github.com>
2023-01-13 09:10:01 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -7,47 +7,48 @@
 	<div class="card card-outline card-primary">
 		<div class="card-header">
 			<div class="card-tools">
-				<a class="btn btn-block btn-sm btn-default btn-flat border-primary new_city" href="javascript:void(0)"><i class="fa fa-plus"></i> Add New</a>
+				<a class="btn btn-block btn-sm btn-default btn-flat border-primary new_establishment" href="javascript:void(0)"><i class="fa fa-plus"></i> Add New</a>
 			</div>
 		</div>
 		<div class="card-body">
 			<table class="table tabe-hover table-bordered" id="list">
-				<colgroup>
+				<!-- <colgroup>
 					<col width="5%">
 					<col width="15%">
-					<col width="15%">
+					<col width="25%">
 					<col width="25%">
 					<col width="15%">
-					<col width="20%">
-				</colgroup>
+					<col width="15%">
+				</colgroup> -->
 				<thead>
 					<tr>
 						<th class="text-center">#</th>
 						<th>Code</th>
-						<th>City</th>
-						<th>Description</th>
-						<th>State</th>
+						<th>Name</th>
+						<th>Address</th>
 						<th>Action</th>
 					</tr>
 				</thead>
 				<tbody>
 					<?php
 					$i = 1;
-					$qry = $conn->query("SELECT c.*,s.name as sname FROM city_list c  inner join state_list s on s.id = c.state_id order by c.name asc ");
+					$qry = $conn->query("SELECT p.*,z.name as zname,c.name as cname, s.name as sname FROM establishment p inner join barangay_list z on z.id = p.zone_id inner join city_list c on c. id= z.city_id inner join state_list s on s.id = c.state_id order by z.name asc ");
 					while($row= $qry->fetch_assoc()):
 					?>
 					<tr>
 						<th class="text-center"><?php echo $i++ ?></th>
 						<td><b><?php echo $row['code'] ?></b></td>
-						<td><b><?php echo $row['name'] ?></b></td>
-						<td><b><?php echo $row['description'] ?></b></td>
-						<td><b><?php echo ucwords($row['sname']) ?></b></td>
+						<td><b><?php echo ucwords($row['name']) ?></b></td>
+						<td><b><?php echo ucwords($row['address'].', '.$row['zname'].', '.$row['cname'].' City, '.$row['sname']) ?></b></td>
 						<td class="text-center">
 		                    <div class="btn-group">
-		                        <a href="javascript:void(0)" data-id='<?php echo $row['id'] ?>' class="btn btn-primary btn-flat manage_city">
+		                    	<a href="javascript:void(0)" data-id='<?php echo $row['id'] ?>' class="btn btn-primary btn-flat view_establishment">
+		                          <i class="fas fa-eye"></i>
+		                        </a>
+		                        <a href="javascript:void(0)" data-id='<?php echo $row['id'] ?>' class="btn btn-primary btn-flat manage_establishment">
 		                          <i class="fas fa-edit"></i>
 		                        </a>
-		                        <button type="button" class="btn btn-danger btn-flat delete_city" data-id="<?php echo $row['id'] ?>">
+		                        <button type="button" class="btn btn-danger btn-flat delete_establishment" data-id="<?php echo $row['id'] ?>">
 		                          <i class="fas fa-trash"></i>
 		                        </button>
 	                      </div>
@@ -60,24 +61,26 @@
 	</div>
 </div>
 <script>
-
 	$(document).ready(function(){
 		$('#list').dataTable()
-		$('.new_city').click(function(){
-			uni_modal("New City","./city/manage.php")
+		$('.new_establishment').click(function(){
+			uni_modal("New Individual","./establishment/manage.php")
 		})
-		$('.manage_city').click(function(){
-			uni_modal("Manage City","./city/manage.php?id="+$(this).attr('data-id'))
+		$('.manage_establishment').click(function(){
+			uni_modal("Manage Individual","./establishment/manage.php?id="+$(this).attr('data-id'))
 		})
-		$('.delete_city').click(function(){
-		_conf("Are you sure to delete this City?","delete_city",[$(this).attr('data-id')])
+		$('.view_establishment').click(function(){
+			uni_modal("Establishment's Details","./establishment/view.php?id="+$(this).attr('data-id'))
+		})
+		$('.delete_establishment').click(function(){
+		_conf("Are you sure to delete this Individual?","delete_establishment",[$(this).attr('data-id')])
 		})
 		$('#list').dataTable()
 	})
-	function delete_city($id){
+	function delete_establishment($id){
 		start_loader()
 		$.ajax({
-			url:_base_url_+'classes/City.php?f=delete',
+			url:_base_url_+'classes/establishment.php?f=delete',
 			method:'POST',
 			data:{id:$id},
 			success:function(resp){
--- manage.php
+++ manage.php
@@ -1,7 +1,7 @@
 <?php 
 require_once('../../config.php');
 if(isset($_GET['id']) && !empty($_GET['id'])){
-	$qry = $conn->query("SELECT * FROM city_list where id = {$_GET['id']}");
+	$qry = $conn->query("SELECT * FROM establishment where id = {$_GET['id']}");
 	foreach($qry->fetch_array() as $k => $v){
 		if(!is_numeric($k)){
 			$$k = $v;
@@ -9,46 +9,92 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
 	}
 }
 ?>
-<form action="" id="city-frm">
+<form action="" id="establishment-frm">
 	<div id="msg" class="form-group"></div>
 	<input type="hidden" name='id' value="<?php echo isset($_GET['id']) ? $_GET['id'] : '' ?>">
 	<div class="form-group">
-		<label for="code" class="control-label">Code</label>
-		<input type="text" class="form-control form-control-sm" name="code" id="code" value="<?php echo isset($code) ? $code : '' ?>" required>
-	</div>
-	<div class="form-group">
-		<label for="name" class="control-label">City/Municipal</label>
+		<label for="name" class="control-label">Name</label>
 		<input type="text" class="form-control form-control-sm" name="name" id="name" value="<?php echo isset($name) ? $name : '' ?>" required>
 	</div>
 	<div class="form-group">
-		<label for="description" class="control-label">Description</label>
-		<textarea type="text" class="form-control form-control-sm" name="description" id="description" required ><?php echo isset($description) ? $description : '' ?></textarea>
+		<label for="address" class="control-label">Address</label>
+		<textarea type="text" class="form-control form-control-sm" name="address" id="address" required ><?php echo isset($address) ? $address : '' ?></textarea>
 	</div>
 	<div class="form-group">
-		<label for="description" class="control-label">State/Province</label>
-		<select name="state_id" id="" class="custom-select custom-select-sm select2">
+		<label for="city_id" class="control-label">City/State</label>
+		<select name="city_id" id="city_id" class="custom-select custom-select-sm select2" required>
+			<option value=""></option>
 			<?php 
-			$state = $conn->query("SELECT * FROM state_list order by name asc");
-			while($row=$state->fetch_assoc()):
+			$city = $conn->query("SELECT c.*,s.name as sname FROM city_list c inner join state_list s on c.state_id=s.id order by c.name asc");
+			while($row=$city->fetch_assoc()):
 			?>
-			<option value="<?php echo $row['id'] ?>" <?php echo isset($state_id) && $state_id == $row['id'] ? "selected" : '' ?>><?php echo ucwords($row['code'].' - '.$row['name']) ?></option>
+			<option value="<?php echo $row['id'] ?>" <?php echo isset($city_id) && $city_id == $row['id'] ? "selected" : '' ?>><?php echo ucwords($row['name'].' City, '.$row['sname']) ?></option>
 			<?php endwhile; ?>
 		</select>
 	</div>
-
+	<div class="form-group">
+		<label for="zone_id" class="control-label">Barangay/Zone</label>
+		<select name="zone_id" id="zone_id" class="custom-select custom-select-sm" required>
+			<option value=""></option>
+			<?php 
+			$zone = $conn->query("SELECT * FROM barangay_list  order by name asc");
+			while($row=$zone->fetch_assoc()):
+			?>
+			<option style="display: none" data-city="<?php echo $row['city_id'] ?>" value="<?php echo $row['id'] ?>" <?php echo isset($zone_id) && $zone_id == $row['id'] ? "selected" : '' ?>><?php echo ucwords($row['name']) ?></option>
+			<?php endwhile; ?>
+		</select>
+	</div>
+	<div class="form-group">
+		<label for="" class="control-label">Image</label>
+		<div class="custom-file">
+          <input type="file" class="custom-file-input rounded-circle" id="customFile" name="img" onchange="displayImg(this,$(this))">
+          <label class="custom-file-label" for="customFile">Choose file</label>
+        </div>
+	</div>
+	<div class="form-group d-flex justify-content-center">
+		<img src="<?php echo validate_image(isset($image_path) ? $image_path : '') ?>" alt="" id="cimg" class="img-fluid img-thumbnail">
+	</div>
 </form>
+<style>
+	img#cimg{
+		height: 15vh;
+		width: 15vh;
+		object-fit: cover;
+		border-radius: 100% 100%;
+	}
+</style>
 <script>
+	function displayImg(input,_this) {
+	    if (input.files && input.files[0]) {
+	        var reader = new FileReader();
+	        reader.onload = function (e) {
+	        	$('#cimg').attr('src', e.target.result);
+	        }
+
+	        reader.readAsDataURL(input.files[0]);
+	    }
+	}
 	$(document).ready(function(){
 		$('.select2').select2();
-		$('#city-frm').submit(function(e){
+		$('#city_id').change(function(){
+			var id = $(this).val();
+			console.log($('#zone_id').find("[data-city='"+id+"']").length)
+			$('#zone_id').find("[data-city='"+id+"']").show()
+		$('#zone_id').select2();
+		})
+		$('#establishment-frm').submit(function(e){
 			e.preventDefault()
 			start_loader()
 			if($('.err_msg').length > 0)
 				$('.err_msg').remove()
 			$.ajax({
-				url:_base_url_+'classes/City.php?f=save',
-				method:'POST',
-				data:$(this).serialize(),
+				url:_base_url_+'classes/Establishment.php?f=save',
+				data: new FormData($(this)[0]),
+			    cache: false,
+			    contentType: false,
+			    processData: false,
+			    method: 'POST',
+			    type: 'POST',
 				error:err=>{
 					console.log(err)
 
@@ -57,10 +103,10 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
 				if(resp == 1){
 					location.reload();
 				}else if(resp == 3){
-					var _frm = $('#city-frm #msg')
-					var _msg = "<div class='alert alert-danger text-white err_msg'><i class='fa fa-exclamation-triangle'></i> Code already exists.</div>"
+					var _frm = $('#establishment-frm #msg')
+					var _msg = "<div class='alert alert-danger text-white err_msg'><i class='fa fa-exclamation-triangle'></i> Person already exists.</div>"
 					_frm.prepend(_msg)
-					_frm.find('input#code').addClass('is-invalid')
+					_frm.find('input#email').addClass('is-invalid')
 					$('[name="code"]').focus()
 				}else{
 					alert_toast("An error occured.",'error');
